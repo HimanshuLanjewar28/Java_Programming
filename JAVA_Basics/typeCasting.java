e1346f901aa04842a97dcedd5e50ce88c52e598a
HimanshuLanjewar28 <hlanjewar17@gmail.com>
2024-05-26 16:32:05 +0530

    typeCasting in JAVA

--- JAVA_Basics/typeCasting.java
+++ JAVA_Basics/typeCasting.java
@@ -4,9 +4,18 @@ import java.util.*;
 public class typeCasting {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        float a = 655.33333333f;
-        int b = (int) a;
-        System.out.println(b);
+        // float a = 655.33333333f;
+        // int b = (int) a;
+        // System.out.println(b);
+
+
+        char ch = 'a';
+        char ch2 = 'l';
+        int number = ch;
+        int num2 = ch2;
+        System.out.println(number);
+        System.out.println(num2);
         sc.close();
     }
 }
+
