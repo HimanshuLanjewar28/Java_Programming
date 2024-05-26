108de9a19fe45ebedad18c8c28e8e081476765bd
HimanshuLanjewar28 <hlanjewar17@gmail.com>
2024-05-26 16:52:30 +0530

    typeConversion in JAVA

---
+++ JAVA_Basics/typeConversion.java
@@ -0,0 +1,16 @@
+package JAVA_Basics;
+
+import java.util.Scanner;
+
+public class typeConversion {
+    public static void main(String[] args) {
+        // int a = 25;
+        // long b = a;
+        // System.out.println(b);
+
+        Scanner sc = new Scanner(System.in);
+        float num = sc.nextInt();
+        System.out.println(num);
+        sc.close();
+    }
+}
