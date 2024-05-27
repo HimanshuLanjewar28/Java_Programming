646d28ae4c1a50f179534012702e002c441c0559
HimanshuLanjewar28 <hlanjewar17@gmail.com>
2024-05-27 16:38:25 +0530

    Java Setup

---
+++ JAVA_Basics/typePromotion.java
@@ -0,0 +1,25 @@
+package JAVA_Basics;
+
+public class typePromotion {
+    public static void main(String[] args) {
+        // char a = 'a';
+        // char b = 'b';
+        // System.out.println((int)a);
+        // System.out.println((int)b);
+        // System.out.println(b - a);
+
+        // short a = 5;
+        // byte b = 25;
+        // char c = 'c';
+        // byte bt = (byte) (a + b + c);
+        // System.out.println(bt);
+
+        int a = 10;
+        float b = 20.25f;
+        long c = 25;
+        double d = 30;
+        double ans = a + b + c + d;
+        System.out.println(ans);
+    }
+}
+
