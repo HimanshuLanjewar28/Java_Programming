3ce22102c31ec488e7a879b980de09168106283c
HimanshuLanjewar28 <hlanjewar17@gmail.com>
2024-05-27 23:35:47 +0530

    relationalOperator

---
+++ JAVA_Basics/Operators/relationalOperator.java
@@ -0,0 +1,14 @@
+package JAVA_Basics.Operators;
+
+public class relationalOperator {
+    public static void main(String [] args){
+        int a = 10;
+        int b = 6;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+    }
+}
