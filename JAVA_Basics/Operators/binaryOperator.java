029a6d12d183ba554fc88c7005c4de6b98cfe6e5
HimanshuLanjewar28 <hlanjewar17@gmail.com>
2024-05-27 23:29:26 +0530

    binaryOperator

---
+++ JAVA_Basics/Operators/binaryOperator.java
@@ -0,0 +1,13 @@
+package JAVA_Basics.Operators;
+
+public class binaryOperator {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
