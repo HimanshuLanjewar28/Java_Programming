aee83699f5cb9ad794e6c1f77e5b1a3d5418f674
HimanshuLanjewar28 <hlanjewar17@gmail.com>
2024-05-28 12:56:04 +0530

    logicalOperator

---
+++ JAVA_Basics/Operators/logicalOperator.java
@@ -0,0 +1,9 @@
+package JAVA_Basics.Operators;
+
+public class logicalOperator {
+    public static void main(String[] args) {
+        System.out.println( (8>5) && (6>2));
+        System.out.println( (8<5) || (5<3));
+        System.out.println( !(8<5));
+    }
+}
