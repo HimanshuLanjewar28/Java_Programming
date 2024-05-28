73c303fe0098d717416eff7b45e41b25fbaf7c94
HimanshuLanjewar28 <hlanjewar17@gmail.com>
2024-05-28 16:46:00 +0530

    if_else_statement

---
+++ JAVA_Basics/conditional_Statements/if_else.java
@@ -0,0 +1,17 @@
+package JAVA_Basics.conditional_Statements;
+
+import java.util.*;
+public class if_else {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the age of the person : ");
+        int age = sc.nextInt();
+        if (age >= 18) {
+            System.out.println("Person can drive and vote!!");
+        }
+        else{
+            System.out.println("Person can't drive and vote!!");
+        }
+        sc.close();
+    }
+}
