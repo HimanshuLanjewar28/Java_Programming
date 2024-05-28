7a214c289d5ce9c7737e803ccbecfe95ee41b58f
HimanshuLanjewar28 <hlanjewar17@gmail.com>
2024-05-28 13:10:07 +0530

    assignmentOperator

---
+++ JAVA_Basics/Operators/assignmentOperator.java
@@ -0,0 +1,22 @@
+package JAVA_Basics.Operators;
+
+public class assignmentOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        a += 10;
+
+        int b = 20;
+        b -= 10;
+
+        int c = 50;
+        c *= 10;
+
+        int d = 60;
+        d/= 10;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
