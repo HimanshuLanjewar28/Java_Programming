c1ff40786f8387fd554fc8181714a4634af926ac
HimanshuLanjewar28 <hlanjewar17@gmail.com>
2024-06-08 14:31:17 +0530

    pass_fail using Ternary Operator

---
+++ JAVA_Basics/practice_Questions/pass_fail_ternaryOperator.java
@@ -0,0 +1,9 @@
+package JAVA_Basics.practice_Questions;
+
+public class pass_fail_ternaryOperator {
+    public static void main( String [] args){
+        int marks = 33;
+        String result = (marks >= 33) ? "Pass" : "Fail";
+        System.out.println(result);
+    }
+}
