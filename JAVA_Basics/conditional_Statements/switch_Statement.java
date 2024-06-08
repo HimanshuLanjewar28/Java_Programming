d744a75e15ad26b31a3001a3c9ae913de7381b3f
HimanshuLanjewar28 <hlanjewar17@gmail.com>
2024-06-08 15:18:13 +0530

    switch Statement

---
+++ JAVA_Basics/conditional_Statements/switch_Statement.java
@@ -0,0 +1,16 @@
+package JAVA_Basics.conditional_Statements;
+
+public class switch_Statement {
+    public static void main(String[] args) {
+        int num = 2;
+        switch(num){
+            case 1 : System.out.println("Himanshu 1");
+            break;
+            case 2 : System.out.println("Himanshu 2");
+            break;
+            case 3 : System.out.println("Himanshu 3");
+            break;
+            default : System.out.println();
+        }
+    }
+}
