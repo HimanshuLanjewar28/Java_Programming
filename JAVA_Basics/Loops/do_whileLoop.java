5b362da8bed68f1cbbafa7bf246227072fdc344e
HimanshuLanjewar28 <hlanjewar17@gmail.com>
2024-06-10 16:49:51 +0530

    do_whileLoop

---
+++ JAVA_Basics/Loops/do_whileLoop.java
@@ -0,0 +1,12 @@
+package JAVA_Basics.Loops;
+
+public class do_whileLoop {
+    public static void main(String[]args){
+        int counter = 0;
+        do{
+            System.out.println(counter);
+            counter ++ ;
+        }
+        while(counter<10);
+    }
+}
