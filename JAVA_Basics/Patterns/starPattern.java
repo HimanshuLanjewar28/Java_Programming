6f8f2491c83c969b2246c8533f146b81aec2fb8e
HimanshuLanjewar28 <hlanjewar17@gmail.com>
2024-06-10 19:35:58 +0530

    starPattern question

--- JAVA_Basics/Patterns/starPattern.java
+++ JAVA_Basics/Patterns/starPattern.java
@@ -3,6 +3,15 @@ package JAVA_Basics.Patterns;
 public class starPattern {
     public static void main(String[]args){
         for(int i=1; i<=4;i++){
+            for(int j=1;j<=(4-i+1);j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println(" ");
+
+        for(int i=1; i<=4;i++){
             for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
