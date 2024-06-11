6d83932fa1ef1078e2f1e5cbd5867b03e960c8e9
HimanshuLanjewar28 <hlanjewar17@gmail.com>
2024-06-11 23:03:55 +0530

    prime no. or Not by using function(question)

--- JAVA_Basics/Functions/primeNum.java
+++ JAVA_Basics/Functions/primeNum.java
@@ -1,10 +1,15 @@
 package JAVA_Basics.Functions;
 
 import java.util.*;
+
 public class primeNum {
 
     public static boolean isPrime(int n) {
         boolean isPrime = true;
+        if (n == 2) {
+            return true;
+        }
+
         for (int i = 2; i <= n - 1; i++) {
             if (n % i == 0) {
                 isPrime = false;
@@ -13,11 +18,11 @@ public class primeNum {
         return isPrime;
     }
 
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            int num = sc.nextInt();
-            boolean primeNum = isPrime(num);
-            System.out.println(primeNum);
-            sc.close();
-        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        boolean primeNum = isPrime(num);
+        System.out.println(primeNum);
+        sc.close();
+    }
 }
