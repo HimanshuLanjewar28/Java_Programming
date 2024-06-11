8de607331954102b126d8357ae7a90b62b0cdbe5
HimanshuLanjewar28 <hlanjewar17@gmail.com>
2024-06-11 23:07:30 +0530

    prime no. or Not by using function(question)

--- JAVA_Basics/Functions/primeNum.java
+++ JAVA_Basics/Functions/primeNum.java
@@ -12,10 +12,10 @@ public class primeNum {
 
         for (int i = 2; i <= n - 1; i++) {
             if (n % i == 0) {
-                isPrime = false;
+                return false;
             }
         }
-        return isPrime;
+        return true;
     }
 
     public static void main(String[] args) {
---
+++ JAVA_Basics/Patterns/hollowRectangle.java
@@ -0,0 +1,12 @@
+package JAVA_Basics.Patterns;
+
+import java.util.*;
+public class hollowRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        for(int i=1; i<=rows;i++){
+            
+        }
+    }
+}
