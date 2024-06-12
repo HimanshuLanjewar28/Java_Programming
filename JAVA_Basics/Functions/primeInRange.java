89ad654e05b8d4b31a7d3770d82a6c941a27efa1
HimanshuLanjewar28 <hlanjewar17@gmail.com>
2024-06-12 11:20:31 +0530

    prime no. or Not by using function(question)

---
+++ JAVA_Basics/Functions/primeInRange.java
@@ -0,0 +1,30 @@
+package JAVA_Basics.Functions;
+
+public class primeInRange {
+    
+    public static boolean isPrime(int n) {
+        if (n == 2) {
+            return true;
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void PrimeNoRange(int n){
+        for(int i=2;i<=n;i++){
+            if(isPrime(i)){
+                System.out.print(i+" ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        PrimeNoRange(20);
+    }
+}
