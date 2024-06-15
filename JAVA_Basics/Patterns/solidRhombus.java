d1aa95c079d521ee5a53589c6965bcf9fdc5f8a1
HimanshuLanjewar28 <hlanjewar17@gmail.com>
2024-06-15 10:59:29 +0530

    Solid Rhombus Pattern (que)

---
+++ JAVA_Basics/Patterns/solidRhombus.java
@@ -0,0 +1,25 @@
+package JAVA_Basics.Patterns;
+import java.util.*;
+
+public class solidRhombus {
+    
+    public static void rhombusPattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        rhombusPattern(rows);
+        sc.close();
+    }
+}
